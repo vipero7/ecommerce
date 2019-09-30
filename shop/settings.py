817a6398d7c5543aa1bed336e1f8943ea0efb934
Pankaj Bhattarai <pankajbhattarai100@gmail.com>
2019-09-30 12:35:35 +0545

    context_processors

--- shop/settings.py
+++ shop/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.context_processors.cart',
             ],
         },
     },
